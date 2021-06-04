a456f9dd8e8a9005de94b8a84bf1c7a67d8265e2
Cyril Mergny <53315179+Cyril-Mergny@users.noreply.github.com>
2021-06-04 14:49:35 +0200

    Import of data from pod yan

--- plot_data.py
+++ plot_data.py
@@ -9,13 +9,14 @@ def PlotPredictions(X, Y, P, batch, mode, title='', name=''):
     len_x = len(X[:, batch, mode])
     ow = Y.shape[0]
     target_len = P.shape[0]
+    X, Y, P = X[:, batch, mode], Y[:, batch, mode], P[:, batch, mode]
     figure, ax = plt.subplots()
-    ax.plot(range(len_x), X[:, batch, mode], label='Data')
-    ax.plot(range(len_x, len_x+ow), Y[:, batch, mode], color='green')
-    ax.plot(range(len_x, len_x+ow), Y[:, batch, mode],'x', color='green',label='target')
-    ax.plot(range(len_x, len_x+target_len), P[:, batch, mode], color='orange')
-    ax.plot(range(len_x, len_x+target_len), P[:, batch, mode], 'x', color='orange', label='predictions')
-    ax.set_ylabel('coeff')
+    ax.plot(range(len_x), X, label='Data')
+    ax.plot(range(len_x, len_x+ow), Y, color='green')
+    ax.plot(range(len_x, len_x+ow), Y,'x', color='green',label='target')
+    ax.plot(range(len_x, len_x+target_len), P, color='orange')
+    ax.plot(range(len_x, len_x+target_len), P, 'x', color='orange', label='predictions')
+    ax.set_ylabel('amplitude')
     ax.set_xlabel('timesteps')
     plt.title(title)
     plt.legend()
@@ -26,7 +27,7 @@ def PlotPredictions(X, Y, P, batch, mode, title='', name=''):
     plt.show()
 
 if __name__ == '__main__':
-
+    print('e')
     # dic = {}
     # for name, param in model.named_parameters():
     #     if param.requires_grad:
@@ -42,25 +43,4 @@ if __name__ == '__main__':
     # plt.colorbar()
     # plt.legend()
     # plt.show()
-    import torch
-    from torch.utils.tensorboard import SummaryWriter
-
-    writer = SummaryWriter()
-    x = torch.arange(-5, 5, 0.1).view(-1, 1)
-    y = -5 * x + 0.1 * torch.randn(x.size())
-
-    model = torch.nn.Linear(1, 1)
-    criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr = 0.1)
-
-    def train_model(iter):
-        for epoch in range(iter):
-            y1 = model(x)
-            loss = criterion(y1, y)
-            writer.add_scalar("Loss/train", loss, epoch)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-    train_model(10)
     
