bbf8850e93db969a7e8d4b31095134c0112b7f30
Cyril Mergny <53315179+Cyril-Mergny@users.noreply.github.com>
2021-06-04 14:50:11 +0200

    import data from pod yann

--- main.py
+++ main.py
@@ -15,29 +15,30 @@ import matplotlib.pyplot as plt
 import set_data as setdata
 import plot_data as plotdata
 
+
 ### MAIN
 # Read and generate dataset
 m = 1
 ow = 20
-data = setdata.ImportData(file_name='Data/podresults_ch180.pickle',  modes=range(m, m+100))
+data = setdata.ImportData(file_name='Data/coeff',  modes=range(m, m+150))
 #data = setdata.GenerateData(L=300, nbr_samples=300)
-#data = setdata.AirQualityData(width=2000)
+#data =  setdata.AirQualityData(width=2000)
 x_train, y_train, x_valid, y_valid = setdata.PrepareDataset(data, noise=None, in_out_stride=(80, ow, 30))
-plt.plot(x_train.to('cpu').detach()[:,0,0])
+#plt.plot(x_train.to('cpu').detach()[:,0,0])
 
 # %% Create and train model
 bs = 16
-model = lstm.LSTM_EncoderDecoder(input_size=x_train.shape[2], hidden_size=30).to(x_train.device)
-loss = lstm.TrainModel(model, x_train, y_train, x_valid, y_valid, n_epochs=200, target_len=ow, batch_size=bs, lr=1e-3, wd=1e-5)
+model = lstm.LSTM_EncoderDecoder(input_size=x_train.shape[2], hidden_size=50).to(x_train.device)
+loss = lstm.TrainModel(model, x_train, y_train, x_valid, y_valid, n_epochs=50, target_len=ow, batch_size=bs, lr=1e-2, wd=1e-5)
 print(torch.cuda.get_device_name(x_train.device))
 plt.plot(np.log10(loss))
 
 # %% Predict&Plot on valid 
 inlen = -0
-batch = 12
+batch = 110
 input_batch = x_valid[inlen:, batch:batch+bs, :]
 p_valid = lstm.Predict(model, input_batch, target_len=ow)
-plotdata.PlotPredictions(input_batch, y_valid[inlen:, batch:batch+bs, :], p_valid, batch=0, mode=0, name='ylabels')
+plotdata.PlotPredictions(input_batch, y_valid[inlen:, batch:batch+bs, :], p_valid, batch=0, mode=0)
 
 
 #%% Predict&Plot on train data
@@ -48,6 +49,6 @@ plotdata.PlotPredictions(x_train[inlen:], y_train, p_train, batch=4, mode=0)
 #path = 'SavedModels/' + input('model name:')
 #torch.save(model.state_dict(), path)
 
-model.load_state_dict(torch.load('SavedModels/ok_iw100ow20'))
+model.load_state_dict(torch.load('SavedModels/overfit_butperfect'))
 model.eval()
 
--- testing.py
+++ testing.py
@@ -2,28 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def MutualInfo(X, Y, bins):
-    """ Computes the mutal information bw two time series """
-    # numpy histo returns a tuple (hist, bins)
-    hist_XY = np.histogram2d(X,Y,bins)[0]
-    hist_X = np.histogram(X,bins)[0]
-    hist_Y = np.histogram(Y,bins)[0]
-
-    H_X = Entropy(hist_X)
-    H_Y = Entropy(hist_Y)
-    H_XY = Entropy(hist_XY)
-    return(H_X+H_Y-H_XY)
-
-def Entropy(hist):
-    # Compute proba from histo
-    p = hist / float(np.sum(hist))
-    # Remove all zeros values
-    p = p[p>0]
-    # Compute Entropy
-    H = -sum(p* np.log2(p))  
-    return(H)
 
 
 
 
-hist = np.histogram2d(a, b, 10)
+a = np.random.rand(100)
+b = np.random.rand(100)
+
+
+hist, _, _ = np.histogram2d(a, b, 10)
+plt.hist2d(a,b)
