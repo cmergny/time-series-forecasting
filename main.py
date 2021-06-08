a90a2ef67bc2a881de832f84bb8ddd632b995b51
Cyril Mergny <53315179+Cyril-Mergny@users.noreply.github.com>
2021-06-08 10:42:39 +0200

    Tested on cavity full time coeff

--- main.py
+++ main.py
@@ -18,24 +18,25 @@ import plot_data as plotdata
 
 ### MAIN
 # Read and generate dataset
-m = 1
+nbr_mods = 10
+start_mod = 1
 ow = 20
-data = setdata.ImportData(file_name='Data/coeff',  modes=range(m, m+150))
+data = setdata.ImportData(file_name="Data/time_coefficient.d", modes=range(start_mod, start_mod + nbr_mods), nbr_snaps=-1)
 #data = setdata.GenerateData(L=300, nbr_samples=300)
 #data =  setdata.AirQualityData(width=2000)
-x_train, y_train, x_valid, y_valid = setdata.PrepareDataset(data, noise=None, in_out_stride=(80, ow, 30))
-#plt.plot(x_train.to('cpu').detach()[:,0,0])
+x_train, y_train, x_valid, y_valid = setdata.PrepareDataset(data, noise=None, in_out_stride=(200, ow, 200))
+plt.plot(x_train.to('cpu').detach()[:,0,0])
 
 # %% Create and train model
-bs = 16
-model = lstm.LSTM_EncoderDecoder(input_size=x_train.shape[2], hidden_size=50).to(x_train.device)
-loss = lstm.TrainModel(model, x_train, y_train, x_valid, y_valid, n_epochs=50, target_len=ow, batch_size=bs, lr=1e-2, wd=1e-5)
+bs = 8
+model = lstm.LSTM_EncoderDecoder(input_size=x_train.shape[2], hidden_size=30).to(x_train.device)
+loss = lstm.TrainModel(model, x_train, y_train, x_valid, y_valid, n_epochs=200, target_len=ow, batch_size=bs, lr=1e-3, wd=1e-7)
 print(torch.cuda.get_device_name(x_train.device))
 plt.plot(np.log10(loss))
 
 # %% Predict&Plot on valid 
 inlen = -0
-batch = 110
+batch = 9
 input_batch = x_valid[inlen:, batch:batch+bs, :]
 p_valid = lstm.Predict(model, input_batch, target_len=ow)
 plotdata.PlotPredictions(input_batch, y_valid[inlen:, batch:batch+bs, :], p_valid, batch=0, mode=0)
@@ -43,7 +44,7 @@ plotdata.PlotPredictions(input_batch, y_valid[inlen:, batch:batch+bs, :], p_vali
 
 #%% Predict&Plot on train data
 p_train = lstm.Predict(model, x_train[inlen:, :bs, :], target_len=ow)
-plotdata.PlotPredictions(x_train[inlen:], y_train, p_train, batch=4, mode=0)
+plotdata.PlotPredictions(x_train[inlen:], y_train, p_train, batch=7, mode=0)
 
 # %% Saving and loading model
 #path = 'SavedModels/' + input('model name:')
